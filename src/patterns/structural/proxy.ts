de6ab441a13bb17f11342ac0526162391615d547
Eugene Kapelko <softwarearchitect19@gmail.com>
2022-08-09 21:11:04 +0300

    Create pattern Proxy

---
+++ src/patterns/structural/proxy.ts
@@ -0,0 +1,35 @@
+interface IPaymentAPI {
+    getPaymentDetail(id: number): IPaymentDetail | undefined;
+}
+
+interface IPaymentDetail {
+    id: number;
+    sum: number;
+}
+
+class PaymentAPI implements IPaymentAPI {
+    private data = [{ id: 1, sum: 10000 }];
+
+    getPaymentDetail(id: number): IPaymentDetail | undefined {
+        return this.data.find(d => d.id === id);
+    }
+}
+
+class PaymentAccessProxy {
+    constructor(private api: PaymentAPI, private userId: number) {}
+
+    getPaymentDetail(id: number): IPaymentDetail | undefined {
+        if (this.userId === 1) {
+            return this.api.getPaymentDetail(id)
+        }
+
+        console.log('Access data payment');
+        return undefined;
+    }
+}
+
+const proxy = new PaymentAccessProxy(new PaymentAPI(), 1);
+console.log(proxy.getPaymentDetail(1));
+
+const proxy2 = new PaymentAccessProxy(new PaymentAPI(), 2);
+console.log(proxy.getPaymentDetail(1));
