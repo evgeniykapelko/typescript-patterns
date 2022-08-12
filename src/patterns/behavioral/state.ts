1d8d697704a9af9cb22e27a0aa3d7f45a9f63385
Eugene Kapelko <softwarearchitect19@gmail.com>
2022-08-12 18:27:32 +0300

    Create pattern State

---
+++ src/patterns/behavioral/state.ts
@@ -0,0 +1,78 @@
+class DocumentItem {
+    public text: string;
+    public state: DocumentItemState;
+
+    constructor() {
+        this.setState(new DraftDocumentItemState());
+    }
+
+    getState() {
+        return this.state;
+    }
+
+    setState(state: DocumentItemState) {
+        this.state = state;
+        this.state.setContext(this);
+    }
+
+    publishDoc() {
+        this.state.publish();
+    }
+
+    deleteDoc() {
+        this.state.delete();
+    }
+}
+
+abstract class DocumentItemState {
+    public name: string;
+    public item: DocumentItem;
+
+    public setContext(item: DocumentItem) {
+        this.item = item;
+    }
+
+    public abstract publish(): void;
+    public abstract delete(): void;
+}
+
+class DraftDocumentItemState extends DocumentItemState {
+    constructor() {
+        super();
+
+        this.name = 'DraftDocument';
+    }
+    
+    public publish(): void {
+        console.log(`send text ${this.item.text}`);
+        this.item.setState(new PublishDocumentItemState());
+    }
+    public delete(): void {
+        console.log(`Document is deleted`)
+    }
+}
+
+class PublishDocumentItemState extends DocumentItemState {
+    constructor() {
+        super();
+
+        this.name = 'PublishDocument';
+    }
+    
+    public publish(): void {
+        console.log(`Can't publish this document`);
+    }
+    public delete(): void {
+        console.log('Move from publish');
+        this.item.setState(new DraftDocumentItemState());
+    }
+}
+
+const item = new DocumentItem();
+item.text = 'My post';
+console.log(item.getState());
+item.publishDoc();
+console.log(item.getState());
+item.publishDoc();
+item.deleteDoc();
+console.log(item.getState());
